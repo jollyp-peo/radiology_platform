c16e1ce751f92a3442007f4bdca4e38cbd2ecee7
Jollyp <erubizzltd@gmail.com>
2025-07-20 11:06:29 +0100

    moved frontend folder to a seperate dir

--- frontend/src/components/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ children }) => {
-  const isLoggedIn = !!localStorage.getItem("token"); // or use context/auth hook
-
-  return isLoggedIn ? children : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;
--- frontend/src/components/Spinner.jsx
+++
@@ -1,5 +0,0 @@
-export default function Spinner() {
-  return (
-    <div className="w-10 h-10 border-4 border-blue-400 border-dashed rounded-full animate-spin"></div>
-  );
-}
